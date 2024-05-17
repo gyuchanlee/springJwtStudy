b899c788b7fe6fc60f3761fddfaa59606ba16504
DoDoBird <88835687+gyuchanlee@users.noreply.github.com>
2024-05-17 20:38:42 +0900

    first

---
+++ src/main/java/com/dodo/jwtreactspring/JwtReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.jwtreactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtReactSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dodo/jwtreactspring/JwtReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.jwtreactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtReactSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
